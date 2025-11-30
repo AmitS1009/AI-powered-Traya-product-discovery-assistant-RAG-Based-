10ea08d1a7aa1971e91b3c5701902084da04f85f
Amit kushwaha <127583900+AmitS1009@users.noreply.github.com>
2025-12-01 03:03:33 +0530

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 # Default to local postgres if not set
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://neondb_owner:npg_2dJrW3HaDhwo@ep-late-darkness-adbnwn98-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require")
+DATABASE_URL = os.getenv("DATABASE_URL", "<add postgreSQL URL>")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
